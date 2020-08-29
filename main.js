2c21d5df299a97e0250cd3653cb4f8e217f333ff
andybyers21 <andy.byers21@gmail.com>
2020-08-29 17:03:28 +0100

    added save data function

--- main.js
+++ main.js
@@ -37,3 +37,36 @@ function fetchIssues() {
       '</div>';
   }
 }
+
+Document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  let issueId = chance.guid();
+  let issueDesc = document.getElementById('issueDescInput').value;
+  let issueSeverity = document.getElementById('issueSeverityInput').value;
+  let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  let issueStatus = 'Open';
+  let issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus,
+  };
+
+  if (localStorage.getItem('issues') === null) {
+    let issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    let issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueInputForm').reset();
+
+  fetchIssues();
+
+  e.preventDefault();
+}
