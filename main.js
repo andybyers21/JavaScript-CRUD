75a1fae3be88d6f1ea0e729a897fc34b0eeee921
andybyers21 <andy.byers21@gmail.com>
2020-08-29 20:33:18 +0100

    added delete issues

--- main.js
+++ main.js
@@ -84,3 +84,17 @@ function setStatusClosed (id) {
 
   fetchIssues();
 }
+
+function deleteIssue(id) {
+  let issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
